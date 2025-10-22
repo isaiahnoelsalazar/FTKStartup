a838ce61a6d33bf74f96ad078d34afaf5deec92b
Isaiah Noel Salazar <isaiahnoelsalazar474@outlook.com>
2025-10-22 19:20:56 +0800

    update: revert all changes

--- MainForm.cs
+++ MainForm.cs
@@ -36,26 +36,9 @@ namespace FTKStartup
                 }
                 if (!File.Exists("envdone"))
                 {
-                    string[] gui = SimpleFileHandler.Read("build/gui.py").Split('\n');
+                    string gui = SimpleFileHandler.Read("build/gui.py");
                     SimpleFileHandler.Write("build/gui.py", "import os\nos.environ['TCL_LIBRARY'] = r'python3_13\\Lib\\site-packages\\tcl\\tcl8.6'\n");
-                    foreach (string line in gui)
-                    {
-                        if (!line.Contains("ASSETS_PATH = OUTPUT_PATH / Path") && !line.Contains("window.geometry"))
-                        {
-                            SimpleFileHandler.Append("build/gui.py", line);
-                        }
-                        else
-                        {
-                            if (line.Contains("ASSETS_PATH = OUTPUT_PATH / Path"))
-                            {
-                                SimpleFileHandler.Append("build/gui.py", "ASSETS_PATH = OUTPUT_PATH / Path(r\"assets\\frame0\")\n");
-                            }
-                            if (line.Contains("window.geometry"))
-                            {
-                                SimpleFileHandler.Append("build/gui.py", "width = 640\nheight = 480\nscreen_width = window.winfo_screenwidth()\nscreen_height = window.winfo_screenheight()\nx = (screen_width // 2) - (width // 2)\ny = (screen_height // 2) - (height // 2)\nwindow.geometry(f\"{width}x{height}+{x}+{y}\")\n");
-                            }
-                        }
-                    }
+                    SimpleFileHandler.Append("build/gui.py", gui);
                     SimpleFileHandler.Write("envdone", string.Empty);
                 }
                 Invoke(new MethodInvoker(() =>
